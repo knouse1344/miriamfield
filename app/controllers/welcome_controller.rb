37af830733dceb5ac2a5a1d68d2330561c0ff5df
Bryan <bknouse@u.rochester.edu>
2013-10-31 22:36:27 -0400

    updated database, schema, and gemfile

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,16 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def services
+  end
+
+  def resources
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Miriam::Application.routes.draw do
+  
+  get "welcome/index"
+  get "welcome/services"
+  get "welcome/about"
+  get "welcome/contact"
+  get "welcome/resources"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root 'welcome#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
