43af37d7c8694a0c85da67e0742bb2468b8ac958
Bryan <bknouse@u.rochester.edu>
2013-10-31 22:28:39 -0400

    first commit new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Miriam::Application.config.secret_key_base = 'f92115a981492ae5e5230f140bd33ab69e9c0ca4b76deb000a9b9226fb6d93430c4a978da7e744e1dd174d5fbedbcaeb8d3e2ecabae00541fe55822c965e3290'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Miriam::Application.config.session_store :cookie_store, key: '_miriam_session'
