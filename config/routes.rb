897e122a76a22d06093da18cccf6b3e786cdef80
Bryan <bknouse@u.rochester.edu>
2013-11-11 22:35:55 -0500

    asdf

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Miriam::Application.routes.draw do
   get "welcome/services"
   get "welcome/about"
   get "welcome/contact"
-  get "welcome/resources"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
